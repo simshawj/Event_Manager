a01821a2e38b27fbec956ed03b851ea9f46c8fe5
James Simshaw <james.simshaw@gmail.com>
2015-03-05 20:00:29 -0800

    Set menu items in the Drawer

--- app/src/main/java/com/jamessimshaw/eventmanager/MainActivity.java
+++ app/src/main/java/com/jamessimshaw/eventmanager/MainActivity.java
@@ -2,16 +2,30 @@ package com.jamessimshaw.eventmanager;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
+    private String[] mDrawerItems;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerItems = getResources().getStringArray(R.array.drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.mainActivityView);
+        mDrawerList = (ListView) findViewById(R.id.eventDrawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, mDrawerItems));
+        //mDrawerList.setOnClickListener(null);
     }
 
 
